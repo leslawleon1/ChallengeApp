2dff285b101c1850fff48fd0cf37f00748917c12
leslawleon1 <leslawleon1@gmail.com>
2023-12-15 20:24:11 +0100

    Zadanie 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+string sex = "kobieta";
+var age = 88;
+
+if (sex == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (sex != "kobieta" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
