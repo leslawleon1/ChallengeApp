0c8d64b5a7251260e94b9c98792f3f719b355826
leslawleon1 <leslawleon1@gmail.com>
2023-12-15 22:10:29 +0100

    Ćwicznia dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,16 +1,18 @@
-﻿string name = "Ewa";
-string sex = "kobieta";
-var age = 88;
+﻿using System;
 
-if (sex == "kobieta" && age < 30)
+int[] grades = new int[365];
+string[] dayOfWeeks = new string[7];
+dayOfWeeks[0] = "poniedziałek";
+dayOfWeeks[1] = "wtorek";
+dayOfWeeks[2] = "środa";
+dayOfWeeks[3] = "czwartek";
+dayOfWeeks[4] = "piątek";
+dayOfWeeks[5] = "sobota";
+dayOfWeeks[6] = "niedziela";
+
+
+
+for(var i=0; i<dayOfWeeks.Length; i++)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
-else if (name == "Ewa" && age == 30)
-{
-    Console.WriteLine("Ewa, lat 30");
-}
-else if (sex != "kobieta" && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
+    Console.WriteLine(dayOfWeeks[i]);
 }
